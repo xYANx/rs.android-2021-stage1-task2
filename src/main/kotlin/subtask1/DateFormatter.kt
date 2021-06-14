7f3914ff3347c9b9c911c47fe6aa7e3804670e81
Yan Navoichyk <yan.navoichyk@lecodegroup.com>
2021-06-14 21:57:43 +0300

    DateFormatter task resolved

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,22 @@
 package subtask1
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val c = Calendar.getInstance()
+        c.isLenient = false
+        return try {
+            c.set(year.toInt(), month.toInt() - 1, day.toInt())
+            val formatter = SimpleDateFormat("dd MMMM, EEEE", Locale("ru"))
+            formatter.format(c.time)
+        } catch (e: Exception) {
+            "Такого дня не существует"
+        }
     }
+
+
 }
