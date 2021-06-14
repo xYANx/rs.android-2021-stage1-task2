173da11af958b921aa2a81d04dd2ffe367c3cac0
Yan Navoichyk <yan.navoichyk@lecodegroup.com>
2021-06-14 22:17:19 +0300

    Abbreviation task resolved

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,16 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var indexA = 0
+        for (charB in b) {
+            while (a[indexA].toUpperCase() != charB) {
+                if (a.length != indexA.inc()) {
+                    indexA++
+                } else {
+                    return "NO"
+                }
+            }
+        }
+        return "YES"
     }
 }
