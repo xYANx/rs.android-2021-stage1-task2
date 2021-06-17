70605ab48640c9bfe44a6a88c645aa5a59ec1afb
Yan Navoichyk <yan.navoichyk@lecodegroup.com>
2021-06-17 21:32:11 +0300

    Blocks task resolved

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,40 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var resultInt = 0
+        var resultString = ""
+        var resultLocalDate = LocalDate.MIN
+
+        for (block in blockA) {
+            when (blockB) {
+                Int::class -> {
+                    if (block is Int)
+                        resultInt += block
+                }
+                String::class -> {
+                    if (block is String)
+                        resultString += block
+                }
+                LocalDate::class -> {
+                    if (block is LocalDate) {
+                        if (resultLocalDate.isBefore(block)) {
+                            resultLocalDate = block
+                        }
+                    }
+                }
+            }
+        }
+        when (blockB) {
+            Int::class -> return resultInt
+            String::class -> return resultString
+            LocalDate::class -> return resultLocalDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+        return Any()
     }
 }
